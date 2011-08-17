63ae44f1935f10df1da4817ad5f253027041d03c
raff <raff@5bac5841-c719-aa4e-b3fe-cce5062f897a>
2011-08-17 12:36:56 +0000

    TRUNK-2394: Can we use the search widget to "list all items" too? https://tickets.openmrs.org/browse/TRUNK-2394
    git-svn-id: http://svn.openmrs.org/openmrs/branches/providers@22629 5bac5841-c719-aa4e-b3fe-cce5062f897a

--- api/src/main/java/org/openmrs/ConceptWord.java
+++ api/src/main/java/org/openmrs/ConceptWord.java
@@ -284,8 +284,12 @@ public class ConceptWord implements java.io.Serializable, Comparable<ConceptWord
 	 * @since 1.8
 	 */
 	public static List<String> getUniqueWords(String phrase, Locale locale) {
-		
-		String[] parts = splitPhrase(phrase);
+		String[] parts;
+		if (phrase.equals("%")) {
+			parts = new String[] { phrase };
+		} else {
+			parts = splitPhrase(phrase);
+		}
 		List<String> uniqueParts = new Vector<String>();
 		
 		if (parts != null) {
